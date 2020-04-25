5c0701d725443cca10dba39daa5ff31843a484ff
honggege <510492728@qq.com>
2020-04-25 16:54:19 +0800

    add CustomerDto

--- BestHouseDomain/src/customer/dto/CustomerDto.java
+++ BestHouseDomain/src/customer/dto/CustomerDto.java
@@ -3,6 +3,8 @@ package customer.dto;
 public class CustomerDto {
 
 	private Long id;
+	
+	private String name;
 
 	public Long getId() {
 		return id;
@@ -10,6 +12,14 @@ public class CustomerDto {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
